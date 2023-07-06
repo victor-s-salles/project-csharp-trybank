9dfe4b64b8f0a0fe0b1fb1edbc5b654168b8ec5e
Victor Salles <victor.salles@engenharia.ufjf.br>
2023-07-06 13:21:10 -0300

    Feat: Requisito 4

--- src/trybank/Trybank.cs
+++ src/trybank/Trybank.cs
@@ -88,7 +88,12 @@ public class Trybank
     // 4. Construa a funcionalidade de checar o saldo
     public int CheckBalance()
     {
-        throw new NotImplementedException();
+        if (!Logged)
+        {
+            throw new AccessViolationException("Usuário não está logado");
+        }
+
+        return Bank[loggedUser, 3];
     }
 
     // 5. Construa a funcionalidade de depositar dinheiro
