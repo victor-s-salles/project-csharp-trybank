a7497c164db4270b43307e44aa06472ec79c2751
Victor Salles <victor.salles@engenharia.ufjf.br>
2023-07-06 13:09:10 -0300

    Feat: Requisito 2

--- src/trybank/Trybank.cs
+++ src/trybank/Trybank.cs
@@ -44,7 +44,30 @@ public class Trybank
     // 2. Construa a funcionalidade de fazer Login
     public void Login(int number, int agency, int pass)
     {
-        throw new NotImplementedException();
+        if (Logged)
+        {
+            throw new AccessViolationException("Usuário já está logado");
+        }
+
+        for (int i = 0; i < registeredAccounts; i++)
+        {
+            if (Bank[i, 0] == number && Bank[i, 1] == agency)
+            {
+                if (Bank[i, 2] == pass)
+                {
+                    Logged = true;
+                    loggedUser = i;
+                }
+                else
+                {
+                    throw new ArgumentException("Senha incorreta");
+                }
+            }
+            else
+            {
+                throw new ArgumentException("Agência + Conta não encontrada");
+            }
+        }
     }
 
     // 3. Construa a funcionalidade de fazer Logout
